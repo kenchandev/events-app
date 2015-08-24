9716cbdb26bb28aac9bb443cd57ab32878d238d1
Ken Chan <KChan923@gmail.com>
2015-08-23 22:59:22 -0400

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 //  Import gulp packages.
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    inject = require(''),
+
+    input = {
+      'javascript': 'public/scripts/*.js'
+    },
+
+    output = {
+      'javascript': 'public/scripts/min'
+    };
 
 //  Create a default task and log a simple message.
 gulp.task('default', function(){
   return gutil.log('Gulp is running!');
+});
+
+gulp.task('watch', function(){
+  gulp.watch()
 });
