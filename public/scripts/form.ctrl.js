a56f474a07f911b6e64615beeb48b728659ab8c8
Ken Chan <KChan923@gmail.com>
2015-08-25 03:31:13 -0400

    modified web form

--- public/scripts/form.ctrl.js
+++ public/scripts/form.ctrl.js
@@ -4,17 +4,18 @@
 (function(){
   /* Define a controller for handling the web form. */
   angular.module('app')
-         .controller('FormController', ['$state', 'EventsService', 'events', 'event', FormController]);
+         .controller('FormController', ['$scope', '$state', 'EventsService', 'events', 'event', FormController]);
 
   /*
    * Two Cases:
    *  - Editing an existing event.
    *  - Adding a new event.
    */
-  function FormController($state, EventsService, events, event){
+  function FormController($scope, $state, EventsService, events, event){
     /* If event.data exists, then the user is editing an existing event. */
     // event.data
-
+    $scope.startTime = new Date();
+    $scope.endTime = new Date();
     /*  */
 
     this.saveEventInfo = function(){
